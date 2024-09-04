a2ecac4d8c07532cbf8da76afc57ea9e0d18d416
Electrom500 <damien.santerne@gmail.com>
2024-09-04 11:15:37 -0400

    preview

--- app/src/main/java/com/example/exercices/MainActivity.kt
+++ app/src/main/java/com/example/exercices/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ExercicesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android Studio",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
