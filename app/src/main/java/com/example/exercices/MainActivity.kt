6fade574dab1a1d1953a6e4c04fb239c7512183e
Electrom500 <damien.santerne@gmail.com>
2024-09-05 10:30:16 -0400

    Fin premier exo

--- app/src/main/java/com/example/exercices/MainActivity.kt
+++ app/src/main/java/com/example/exercices/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.exercices.ui.theme.ExercicesTheme
 
@@ -21,12 +25,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ExercicesTheme {
-                /*Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = ", enjoying the app ? ",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }*/
+                GreetingText("Salut à toi !\nJ'espère que tu passes un bon moment. ",
+                    "Damien",
+                    modifier = Modifier.padding(16.dp))
             }
         }
     }
@@ -35,16 +36,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
 
-    Column {
+    Column(verticalArrangement = Arrangement.Center, modifier = modifier) {
         Text(
             text = message,
-            fontSize = 50.sp,
-            lineHeight = 115.sp,
-            modifier = modifier
+            fontSize = 40.sp,
+            lineHeight = 80.sp,
+            textAlign = TextAlign.Center
         )
         Text(
             text = "$from :D",
-            fontSize = 25.sp
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
         )
     }
 }
@@ -57,6 +61,6 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
 @Composable
 fun GreetingPreview() {
     ExercicesTheme {
-        GreetingText("Salut à toi !\nJ'espère que tu passe un bon moment. ","Damien")
+        GreetingText("Salut à toi !\nJ'espère que tu passes un bon moment. ","Damien")
     }
 }
