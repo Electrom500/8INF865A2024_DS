668e45ed45ec266058352d1710aeae661b455e89
Electrom500 <damien.santerne@gmail.com>
2024-09-05 11:02:37 -0400

    only background

--- app/src/main/java/com/example/exercices/MainActivity.kt
+++ app/src/main/java/com/example/exercices/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.exercices
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,11 +15,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.exercices.ui.theme.ExercicesTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,14 +58,24 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = "'Sup"
+    )
+}
+
 @Preview(
     showBackground = true,
-    showSystemUi = true,
-    name = "My Preview"
+    showSystemUi = true
 )
 @Composable
-fun GreetingPreview() {
+fun GreetingPreview(modifier: Modifier = Modifier) {
     ExercicesTheme {
-        GreetingText("Salut à toi !\nJ'espère que tu passes un bon moment. ","Damien")
+        //GreetingText("Salut à toi !\nJ'espère que tu passes un bon moment. ","Damien")
+        GreetingImage("Salut à toi !\nJ'espère que tu passes un bon moment. ","Damien")
     }
 }
+
