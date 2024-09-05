91094c01e14c00229dfabb8e0861e7288bfdc948
Electrom500 <damien.santerne@gmail.com>
2024-09-05 12:03:47 -0400

    end of exercises expected for 2nd lesson

--- app/src/main/java/com/example/exercices/MainActivity.kt
+++ app/src/main/java/com/example/exercices/MainActivity.kt
@@ -64,7 +64,8 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
             fontSize = 20.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                //.align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
