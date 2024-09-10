81bc501af9859729bc938fe14e9909f421bcc5bb
Electrom500 <damien.santerne@gmail.com>
2024-09-10 15:44:19 -0400

    fin de td

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -86,8 +86,9 @@ fun TipTimeLayout() {
 
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
+    val amountInput = "0"
     TextField(
-        value = "",
+        value = amountInput,
         onValueChange = {},
         modifier = modifier
     )
